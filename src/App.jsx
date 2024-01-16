559b8c459a1dd0e0523769d8e0363b3df00cf335
KMalif <khafidulmualif2019@gmail.com>
2024-01-16 17:08:19 +0700

    add theme on app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
+import { useContext } from "react";
+import { ThemeContext } from "./context/ThemeContext";
 
 
 function App() {
+  const {theme} = useContext(ThemeContext)
 
   return (
-   <div>
+   <div className={theme}>
       <Routes>
         <Route index element={<Home/>}/>
       </Routes>
